928710b3a4dcc29327228bfc4a61afbbd7c0706f
andrericci00 <aricci2000@gmail.com>
2023-03-06 15:03:02 +0100

    Aggiunti ulteriori metodi di scrittura e salvataggio file

--- Transaction.h
+++ Transaction.h
@@ -16,7 +16,13 @@ public:
     float getImporto() const;
     string getDescription() const;
     virtual string getType() const = 0;
+    void print() const;
+    virtual void save(ofstream& file) const = 0;
+    virtual void load(ifstream& file) = 0;
 
+protected:
+    float m_money;
+    string m_description;
 
 };
 
