7eede5ecd42f3d0d7068f49f183cd1a5c7ee37bb
andrericci00 <aricci2000@gmail.com>
2023-03-06 15:09:03 +0100

    Aggiunti metodi importo e descrizione

--- Transaction.cpp
+++ Transaction.cpp
@@ -7,3 +7,10 @@
 Transaction::Transaction(): m_money() , m_description() {}
 Transaction::Transaction(float importo, const string& description) : m_money(importo), m_description(description) {}
 
+float Transaction::getImporto() const {
+    return m_money;
+}
+
+string Transaction::getDescription() const {
+    return m_description;
+}
