7814f8e85436aefabd490a0409c9fd1759857887
andrericci00 <aricci2000@gmail.com>
2023-03-06 16:40:05 +0100

    esempio di main aggiunto

--- main.cpp
+++ main.cpp
@@ -7,4 +7,30 @@ using namespace std;
 #include "Transaction_out.h"
 
 //esempio utilizzo classi
-int main() {}
+int main() {
+    std::vector<Transaction*> transazioni;
+    transazioni.push_back(new Transaction_in(100.0, "Stipendio"));
+    transazioni.push_back(new Transaction_out(50.0, "Spesa al supermercato"));
+    transazioni.push_back(new Transaction_out(20.0, "Benzina"));
+
+    std::ofstream file("transazioni.txt");
+    if (!file) {
+        std::cerr << "Errore durante l'apertura del file di output" << std::endl;
+        return 1;
+    }
+
+    for (auto t : transazioni) {
+        t->save(file);
+    }
+
+    file.close();
+
+    for (auto t : transazioni) {
+        delete t;
+    }
+
+    return 0;
+}
+
+
+
