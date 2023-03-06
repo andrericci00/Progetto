bda99e12f3deb7069446564f5e561a8409af67b3
andrericci00 <aricci2000@gmail.com>
2023-03-06 15:47:00 +0100

    Definizioni Transaction_in.cpp

--- Transaction_in.cpp
+++ Transaction_in.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "Transaction_in.h"
+Transaction_in::Transaction_in() :Transaction() {}
+Transaction_in::Transaction_in(float importo, const string &description) : Transaction(importo,description){}
+string Transaction_in::getType() const {
+    return "Entrata";
+}
+
+void Transaction_in::save(ofstream &file) const {
+    file << getType << endl;
+    file <<m_money<< endl;
+    file << m_description << endl;
+}
+
+void Transaction_in::load(ifstream& file) {
+    file >> m_money;
+    file.ignore();
+    getline(file, m_description);
+}
