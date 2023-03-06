8a758a91756d08171122e849b88f29dc54a65d2a
andrericci00 <aricci2000@gmail.com>
2023-03-06 14:56:28 +0100

    Creata classe Transaction e metodo getImporto

---
+++ Transaction.h
@@ -0,0 +1,21 @@
+//
+// Created by 182913 on 06/03/2023.
+//
+
+#ifndef LABORATORIO_TRANSACTION_H
+#define LABORATORIO_TRANSACTION_H
+#include <fstream>
+#include <string>
+using namespace std;
+
+class Transaction {
+public:
+    Transaction();
+    Transaction(float importo, const string& description);
+
+    float getImporto() const;
+
+};
+
+
+#endif //LABORATORIO_TRANSACTION_H
