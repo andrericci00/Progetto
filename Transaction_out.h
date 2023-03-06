5bf27996ff46d0946a9e5eb00b08ac76b65b2b0f
andrericci00 <aricci2000@gmail.com>
2023-03-06 15:50:59 +0100

    Creazione classe Transaction_out

---
+++ Transaction_out.h
@@ -0,0 +1,14 @@
+//
+// Created by 182913 on 06/03/2023.
+//
+
+#ifndef LABORATORIO_TRANSACTION_OUT_H
+#define LABORATORIO_TRANSACTION_OUT_H
+
+
+class Transaction_out {
+
+};
+
+
+#endif //LABORATORIO_TRANSACTION_OUT_H
