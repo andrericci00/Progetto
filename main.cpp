b98ecb8a802c9bfe2094a7fdb1cae9a66f1f1d33
andrericci00 <aricci2000@gmail.com>
2023-03-16 15:16:54 +0100

    Cambiato nome file

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 //esempio utilizzo classi
 int main() {
     std::vector<Transaction *> transactions;
-    ifstream file("transazioni.txt");
+    ifstream file("transactions.txt");
     if (file.is_open()) {
         while (!file.eof()) {
             string type;
